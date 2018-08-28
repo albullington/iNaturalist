a1834bd1d2b4479ef30b3ca48e6059c6de85416a
Amanda Bullington <amanda@albull.us>
2018-08-28 10:55:18 -0500

    Added GET request and ability to play sounds

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    <p>React is compiling correctly</p>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.playSound = this.playSound.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchObservations();
+  }
+
+  fetchObservations() {
+    const url = 'https://api.inaturalist.org/v1/observations';
+    const params = '?order=desc&order_by=created_at&sounds=true&per_page=100'
+    axios.get(url + params)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }
+
+  playSound() {
+    const audio = new Audio('https://static.inaturalist.org/sounds/25393.x-m4a?1535421964');
+    audio.loop = true;
+    audio.play();
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.playSound}>Play sound</button>
+      </div>
+    );
+  }
+}
 
 export default App;
